813df454613ebf8b92b33510bee46f56a11c8d2c
Aaron Liao <aliao4@uci.edu>
2017-02-07 16:27:55 -0800

    copied lab1 to 2, added description

---
+++ lab2/verif/top.sv
@@ -0,0 +1,19 @@
+module top(
+    input  logic clk, reset,
+    output logic [31:0] DataAdr,
+    output logic [31:0] WriteData,
+    output logic MemWrite
+    );
+
+    logic [31:0] PC, Instr, ReadData;
+
+
+    // instantiate processor and memories
+    arm  arm(clk, reset, PC, Instr, MemWrite, DataAdr, WriteData, ReadData);
+	// arm  arm(clk, reset, PC, Instr, MemWrite, ALUResult, WriteData, ReadData);
+	
+    imem imem(PC, Instr);
+    
+    dmem dmem(clk, MemWrite, DataAdr, WriteData, ReadData);
+    
+endmodule
