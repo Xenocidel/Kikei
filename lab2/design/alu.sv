5a0e471c66e8424ab7aa2e7a4c875e58ca7eb693
Aaron Liao <aliao4@uci.edu>
2017-02-10 17:20:29 -0800

    partial progress alu

--- lab2/design/alu.sv
+++ lab2/design/alu.sv
@@ -1,7 +1,7 @@
 module alu(
     input logic [31:0] A, B,
-    //input logic [3:0] ALUControl,
-	input logic [1:0] ALUControl,
+    input logic [3:0] ALUControl,
+	//input logic [1:0] ALUControl,
 	output logic [31:0] ALUResult,
 	output logic [3:0] ALUFlags
     );
@@ -16,31 +16,27 @@ module alu(
         ALUResult = 32'd0;
 		N = ALUResult[31];
 		V = 1'b0;
-		C = 1'b0;
+		//C = 1'b0;
 		Z = ~|ALUResult;
 		
         case(ALUControl)
-            // 4'b0000 :  // No OP
-            // begin
-                // ALUResult = 32'd0;
-				// N = 1'b0;
-                // C = 1'b0;
-                // V = 1'b0;
-                // Z = 1'b0;
-            // end
-            2'b00 :   // Add
+			4'b0000 :   // AND
             begin
-                {C,ALUResult} = A + B;
-                if (A[31] & B[31] & ~ALUResult[31])
-                    V = 1'b1;
-                else if (~A[31] & ~B[31] & ALUResult[31])
-                    V = 1'b1;
-                else
-                    V = 1'b0;
-                Z = 1'b0;
-				N = ALUResult[31];
+                ALUResult = A & B;
+                C = 1'b0;
+                V = 1'b0;
+                Z = ~|ALUResult;
+				N = 1'b0;
             end
-            2'b01 :   // SUB
+			4'b0001 :   // XOR
+            begin
+                ALUResult = A ^ B;
+                C = 1'b0;
+                V = 1'b0;
+                Z = ~|ALUResult;
+				N = 1'b0;
+            end
+			4'b0010 :   // SUB
             begin
                 {C,ALUResult} = A - B;
                 if (A[31] & ~B[31] & ~ALUResult[31])
@@ -49,7 +45,67 @@ module alu(
                     V = 1'b1;
                 else
                     V = 1'b0;
-                Z = 1'b0;
+                Z = ~|ALUResult;
+				N = ALUResult[31];
+            end
+			4'b0011 :   // RSB (Reverse Sub)
+            begin
+                {C,ALUResult} = B - A;
+                if (B[31] & ~A[31] & ~ALUResult[31])
+                    V = 1'b1;
+                else if (~B[31] & A[31] & ALUResult[31])
+                    V = 1'b1;
+                else
+                    V = 1'b0;
+                Z = ~|ALUResult;
+				N = ALUResult[31];
+            end
+            4'b0100 :   // Add
+            begin
+                {C,ALUResult} = A + B;
+                if (A[31] & B[31] & ~ALUResult[31])
+                    V = 1'b1;
+                else if (~A[31] & ~B[31] & ALUResult[31])
+                    V = 1'b1;
+                else
+                    V = 1'b0;
+                Z = ~|ALUResult;
+				N = ALUResult[31];
+            end
+			4'b0101 :   // Add with Carry
+            begin
+                {C,ALUResult} = A + B + C;
+                if (A[31] & B[31] & ~ALUResult[31])
+                    V = 1'b1;
+                else if (~A[31] & ~B[31] & ALUResult[31])
+                    V = 1'b1;
+                else
+                    V = 1'b0;
+                Z = ~|ALUResult;
+				N = ALUResult[31];
+            end
+			4'b0110 :   // Sub with Carry
+            begin
+                {C,ALUResult} = A - B - C;
+                if (A[31] & ~B[31] & ~ALUResult[31])
+                    V = 1'b1;
+                else if (~A[31] & B[31] & ALUResult[31])
+                    V = 1'b1;
+                else
+                    V = 1'b0;
+                Z = ~|ALUResult;
+				N = ALUResult[31];
+            end
+			4'b0111 :   // Reverse Sub with Carry
+            begin
+                {C,ALUResult} = B - A - C;
+                if (B[31] & ~A[31] & ~ALUResult[31])
+                    V = 1'b1;
+                else if (~B[31] & A[31] & ALUResult[31])
+                    V = 1'b1;
+                else
+                    V = 1'b0;
+                Z = ~|ALUResult;
 				N = ALUResult[31];
             end
             // 4'b0011 :   // COMP
@@ -63,33 +119,17 @@ module alu(
                     // Z = 1'b0;
 				// N = 1'b0;
             // end
-            2'b10 :   // AND
-            begin
-                ALUResult = A & B;
-                C = 1'b0;
-                V = 1'b0;
-                Z = 1'b0;
-				N = 1'b0;
-            end
-            2'b11 :   // OR
+            4'b1100 :   // OR
             begin
                 ALUResult = A | B;
                 C = 1'b0;
                 V = 1'b0;
-                Z = 1'b0;
+                Z = ~|ALUResult;
 				N = 1'b0;
             end
             // 4'b0111 :   // NOT
             // begin
                 // ALUResult = ~A;
-                // C = 1'b0;
-                // V = 1'b0;
-                // Z = 1'b0;
-				// N = 1'b0;
-            // end
-            // 4'b1000 :   // XOR
-            // begin
-                // ALUResult = A ^ B;
                 // C = 1'b0;
                 // V = 1'b0;
                 // Z = 1'b0;
