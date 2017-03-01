2d4607962fec0cb1a7ec62f86f4d26c31202669f
Bryan Ou <bryanou96@gmail.com>
2017-02-28 21:59:18 -0800

    Final Submission for Lab 2.
    We did about 90% of what was asked.
    Only things missing/errors are:
    1. log error with TST command.
    2. STRH LDRH
    3. Synthesis needs to be debugged, but gives us the required values the
    professor wants.

---
+++ Lab 2 Submission/Lab2/design/controller.sv
@@ -0,0 +1,24 @@
+module controller(input logic clk, reset,
+				input logic [31:0] Instr,
+				input logic [3:0] ALUFlags,
+				output logic [1:0] RegSrc,
+				output logic RegWrite,
+				output logic [1:0] ImmSrc,
+				output logic ALUSrc,
+				output logic [3:0] ALUControl,
+				output logic MemWrite, MemtoReg,
+				output logic PCSrc,
+				logic [2:0] ShiftOp,
+				logic wr14,
+				logic PrevC,
+				output logic be
+				);
+logic [1:0] FlagW;
+logic 		PCS, RegW, MemW;
+decoder dec(Instr[27:26], Instr[6:5], Instr[25:20], Instr[15:12], Instr[11:0],
+			FlagW, PCS, RegW, MemW,
+			MemtoReg, ALUSrc, ImmSrc, RegSrc, ALUControl, ShiftOp, wr14, be);
+condlogic cl(clk, reset, Instr[31:28], ALUFlags,
+			FlagW, PCS, RegW, MemW,
+			PCSrc, RegWrite, MemWrite, PrevC);
+endmodule
---
+++ Lab 2 Submission/Lab2/verif/tb_top.sv
@@ -0,0 +1,61 @@
+
+module tb_top();
+    logic clk;
+    logic reset;
+    logic [31:0] DataAdr;
+    logic [31:0] WriteData;
+    logic MemWrite;
+
+
+    // instantiate device to be tested
+    top dut(clk, reset, DataAdr, WriteData, MemWrite);
+
+
+    // initialize test
+    initial
+    begin
+        reset <= 1; # 22; reset <= 0;
+    end
+
+    // generate clock to sequence tests
+    always
+    begin
+        clk <= 1; # 5; clk <= 0; # 5;
+    end
+
+	int clk_cnt;
+	always @(posedge clk) begin
+		clk_cnt++;
+		if (clk_cnt > 300) begin
+			for (int i = 0; i<16; i++) begin
+				$display("%g %x", i, tb_top.dut.arm.dp.rf.rf[i]);
+			end
+			$finish;
+		end
+	end
+	
+    // check that 7 gets written to address 0x64
+    // at end of program
+    // always @(negedge clk)
+    // begin
+        // if(MemWrite) begin
+            // if(DataAdr === 252 &  WriteData === 22) 
+            // begin
+                // $display("Simulation succeeded");
+                // $stop;
+            // end 
+            // else //if (DataAdr !== 96) 
+            // begin
+                // $display("Simulation failed");
+                // $display("your score is %d out of 22", WriteData);
+				// $stop;
+            // end
+        // end
+    // end
+
+    // Limits sim time to 30000ns
+    initial begin
+    #30000;
+    $finish;
+    end
+endmodule
