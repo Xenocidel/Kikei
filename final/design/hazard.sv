9d994495cc925fe3bbd46566354b93eddc259ac7
Aaron Liao <aliao4@uci.edu>
2017-03-16 11:03:35 -0700

    added hazard unit framework

---
+++ final/design/hazard.sv
@@ -0,0 +1,13 @@
+module hazard(input  logic			MemtoRegE,
+			  input  logic			RegWriteW,
+			  input  logic			RegWriteM,
+			  input  logic			Match,
+			  output logic			ForwardAE,
+			  output logic			ForwardBE,
+			  output logic			FlushE,
+			  output logic			StallD,
+			  output logic			StallF);
+	
+	
+	
+endmodule
