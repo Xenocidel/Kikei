c4bb5c3c96d59d6c768cab670679dfd13ab652f6
kelvinhpwebmaster <kelvinhp@uci.edu>
2017-03-24 21:23:13 -0700

    fixed shift problems

--- final/design/arm.sv
+++ final/design/arm.sv
@@ -12,17 +12,21 @@ module arm(input logic clk, reset,
 	logic PCSrcM, RegWriteM, MemtoRegM;
 	logic PCSrcW, RegWriteW, MemtoRegW;
 	logic [1:0] RegSrcD, ImmSrcD, RegSrcE, ImmSrcE;
-	logic [2:0] ShiftOp;
+	logic [2:0] ShiftOpD;
 	logic wr14;
 	logic PrevC;
+
+	logic[31:0] InstrE;
+	logic[2:0] ShiftOpE;
+
 	controller c(clk, reset, Instr, ALUFlags,
 			RegSrcD, RegWriteD, ImmSrcD,
 			ALUSrcD, ALUControlD,
-			MemWriteD, MemtoRegD, PCSrcD, ShiftOp, wr14, PrevC, be);
+			MemWriteD, MemtoRegD, PCSrcD, ShiftOpD, wr14, PrevC, be);
 
 	idexe idexe(clk, RegSrcD, RegWriteD, ImmSrcD, ALUSrcD, ALUControlD,
-		MemWriteD, MemtoRegD, PCSrcD, RegSrcE, RegWriteE, ImmSrcE, ALUSrcE,
-		ALUControlE, MemWriteE, MemtoRegE, PCSrcE);
+		MemWriteD, MemtoRegD, PCSrcD, ShiftOpD, Instr, RegSrcE, RegWriteE, ImmSrcE, ALUSrcE,
+		ALUControlE, MemWriteE, MemtoRegE, PCSrcE,ShiftOpE, InstrE);
 
 	exemem exemem(clk,PCSrcE, RegWriteE, MemtoRegE, MemWriteE, PCSrcM, RegWriteM, MemtoRegM, MemWriteM);
 	memwb memwb(clk, PCSrcM, RegWriteM, MemtoRegM, PCSrcW, RegWriteW, MemtoRegW);
@@ -31,6 +35,6 @@ module arm(input logic clk, reset,
 			RegSrcE, RegWriteE, ImmSrcE,
 			ALUSrcE, ALUControlE,
 			MemtoRegE, PCSrcD,
-			ALUFlags, PC, Instr,
-			ALUResult, WriteData, ReadData, ShiftOp, wr14, PrevC);
+			ALUFlags, PC, InstrE,
+			ALUResult, WriteData, ReadData, ShiftOpE, wr14, PrevC);
 endmodule
--- final/design/idexe.sv
+++ final/design/idexe.sv
@@ -6,16 +6,22 @@ module idexe(input logic clk,
   input logic [3:0] ALUControlD,
   input logic MemWriteD, MemtoRegD,
   input logic PCSrcD,
+  input logic [2:0] ShiftOpD,
+  input logic [31:0] InstrD,
   output logic[1:0] RegSrcE,
   output logic RegWriteE,
   output logic [1:0]ImmSrcE,
   output logic ALUSrcE,
   output logic [3:0] ALUControlE,
   output logic MemWriteE, MemtoRegE,
-  output logic PCSrcE);
+  output logic PCSrcE,
+  output logic [2:0] ShiftOpE,
+  output logic [31:0] InstrE);
 
   always@(posedge clk)
   begin
+  InstrE <= InstrD;
+  ShiftOpE <= ShiftOpD;
   RegSrcE <=RegSrcD;
   RegWriteE <= RegWriteD;
   ImmSrcE <= ImmSrcD;
